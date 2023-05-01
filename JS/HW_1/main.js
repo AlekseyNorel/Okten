e3707a2897d0b03924203169fb84a9bf05bed9b7
AlekseyNorel <aleksei.julivi@gmail.com>
2023-05-01 21:51:32 +0300

    JS HW_1

---
+++ JS/HW_1/main.js
@@ -0,0 +1,79 @@
+////////////  Task №1  ///////////////
+
+const a = 'hello';
+console.log(a);
+const b = 'owu';
+console.log(b);
+const c = 'com';
+console.log(c);
+const d = 'ua';
+console.log(d);
+const e = 1;
+console.log(e);
+const f = 10;
+console.log(f);
+const g = -999;
+console.log(g);
+const h = 123;
+console.log(h);
+const i = 3.14;
+console.log(i);
+const j = 'hello';
+console.log(j);
+const k = 16;
+console.log(k);
+const l = true;
+console.log(l);
+const m = false;
+console.log(m)
+
+////////////  Task №2  ///////////////
+
+const firstName = 'Oleksii';
+const middleName = 'Sergiyovich';
+const lastName = 'Norel';
+const person = `${firstName} ${middleName} ${lastName}`
+console.log(person);
+
+////////////  Task №3  ///////////////
+
+let a1 = 100;
+let b1 = '100';
+let c1 = true;
+console.log(typeof a1);
+console.log(typeof b1);
+console.log(typeof c1);
+
+////////////  Task №4  ///////////////
+
+let name = prompt('Enter name');
+let secondName = prompt('Enter second name');
+let nameLast = prompt('Enter last name');
+console.log(`${name} ${secondName} ${nameLast}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
